579060d9eed1c0eb36e105c5011a6185ff8f62c8
Bora Kilicoglu <sbkilicoglu@gmail.com>
2020-11-24 18:16:50 +0100

    Safari crypto issue fixed

--- src/views/pages/ecommerce/Inventory.js
+++ src/views/pages/ecommerce/Inventory.js
@@ -59,49 +59,53 @@ export default function Inventory(props) {
   ));
 
   return (
-    <div>
-      <div className="flex bg-white w-full px-8 py-10">
-        <h2 className="text-3xl font-bold pb-0">Inventory</h2>
-        <div className="flex ml-auto items-center">
-          <div className="relative">
-            <input
-              onChange={handleChange}
-              className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 pr-4 pl-10 block w-full appearance-none leading-normal"
-              type="email"
-              placeholder="Search products"
-            />
-            <div className="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
-              <svg
-                className="fill-current pointer-events-none text-gray-600 w-4 h-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
-              </svg>
+    <div className="relative min-h-full flex-auto">
+      <div className="flex flex-col w-full absolute inset-0 overflow-hidden">
+        <div className="overflow-scroll">
+          <div className="flex bg-white w-full px-8 py-10">
+            <h2 className="text-3xl font-bold pb-0">Inventory</h2>
+            <div className="flex ml-auto items-center">
+              <div className="relative">
+                <input
+                  onChange={handleChange}
+                  className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 pr-4 pl-10 block w-full appearance-none leading-normal"
+                  type="email"
+                  placeholder="Search products"
+                />
+                <div className="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
+                  <svg
+                    className="fill-current pointer-events-none text-gray-600 w-4 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
+                  </svg>
+                </div>
+              </div>
+              <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-normal py-2 pr-4 pl-3 rounded inline-flex items-center ml-3">
+                <Icon fontSize="small">add</Icon>
+                <span className="ml-2">Add</span>
+              </button>
             </div>
           </div>
-          <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-normal py-2 pr-4 pl-3 rounded inline-flex items-center ml-3">
-            <Icon fontSize="small">add</Icon>
-            <span className="ml-2">Add</span>
-          </button>
+          <div>
+            <table className="table-auto w-full bg-white border-t">
+              <thead className="text-xs text-gray-600 bg-gray-100">
+                <tr className="border-b text-left">
+                  <th className="pl-24 py-4 font-medium">
+                    <span className="ml-4">SKU</span>
+                  </th>
+                  <th className="px-4 py-4 font-medium">Name</th>
+                  <th className="px-4 py-4 font-medium">Price</th>
+                  <th className="px-4 py-4 font-medium">Stock</th>
+                  <th className="px-4 py-4 font-medium">Active</th>
+                  <th className="px-4 py-4 font-medium">Details</th>
+                </tr>
+              </thead>
+              <tbody>{productList}</tbody>
+            </table>
+          </div>
         </div>
-      </div>
-      <div>
-        <table className="table-auto w-full bg-white border-t">
-          <thead className="text-xs text-gray-600 bg-gray-100">
-            <tr className="border-b text-left">
-              <th className="pl-24 py-4 font-medium">
-                <span className="ml-4">SKU</span>
-              </th>
-              <th className="px-4 py-4 font-medium">Name</th>
-              <th className="px-4 py-4 font-medium">Price</th>
-              <th className="px-4 py-4 font-medium">Stock</th>
-              <th className="px-4 py-4 font-medium">Active</th>
-              <th className="px-4 py-4 font-medium">Details</th>
-            </tr>
-          </thead>
-          <tbody>{productList}</tbody>
-        </table>
       </div>
     </div>
   );
