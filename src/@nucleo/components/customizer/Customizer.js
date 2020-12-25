42b824884e1dbe7e0af9123c1a228bcc2c6f09c0
Bora Kilicoglu <sbkilicoglu@gmail.com>
2020-12-25 13:20:55 +0100

    Customizer updated

--- src/@nucleo/components/customizer/Customizer.js
+++ src/@nucleo/components/customizer/Customizer.js
@@ -17,7 +17,7 @@ import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormControl from "@material-ui/core/FormControl";
 
-import emptyLight from "../../../assets/images/demo/layouts/empty-light.png";
+// import emptyLight from "../../../assets/images/demo/layouts/empty-light.png";
 import basicLight from "../../../assets/images/demo/layouts/nucleo-basic-light.png";
 
 const useStyles = makeStyles({
